b2baa51da94dead9327f78119b55f44c606ed721
jsifalda <sifalda.jiri@gmail.com>
2019-01-19 16:03:10 +0100

    Simplify getByIdSelector, remove third party dependencies

--- src/getByIdSelector.js
+++ src/getByIdSelector.js
@@ -1,21 +1,6 @@
-import isObject from 'is-plain-object'
-import { defaultMemoize } from 'reselect'
-import idhash from 'idhash'
-
-const hash = (key) => {
-  return defaultMemoize((data) => {
-    if (!isObject(data)) {
-      data = idhash(data, key)
-    }
-
-    return data
-  })
-}
-
-const getByIdSelector = (selector, key = 'id') => {
+const getByIdSelector = (selector) => {
   return (state, { id }) => {
-    let data = selector(state)
-    return (hash(key)(data) || {})[id]
+    return (selector(state) || {})[id]
   }
 }
 
